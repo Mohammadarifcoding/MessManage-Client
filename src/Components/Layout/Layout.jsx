9afac8c75fe344a53f4035cd71ca799e12698e27
Mohammad Arif <programmermdarif@gmail.com>
2024-02-18 07:50:41 +0600

    Completed making the layout for mealmanage

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -6,7 +6,7 @@ import Footer from "../Shared/Footer/Footer";
 import Container from "../Shared/Container/Container";
 import Drawer from "react-modern-drawer";
 import "react-modern-drawer/dist/index.css";
-
+import { FaArrowRight } from "react-icons/fa6";
 const Layout = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleDrawer = () => {
@@ -14,9 +14,11 @@ const Layout = () => {
   };
   return (
     <div className="container mx-auto max-w-[1600px]">
-      <div className="bg-primary w-full">
+      <div className="bg-primary w-full xl:hidden block">
         <Container>
-          <div className="flex justify-between  items-center ">
+
+            {/* Navbar */}
+          <div className=" flex justify-between  items-center ">
             {/* Navbar */}
             <div className=" px-3 py-4  text-fourth">
               <div className="flex gap-3 items-center">
@@ -42,10 +44,99 @@ const Layout = () => {
               </div>
             </div>
           </div>
-        </Container>
-      </div>
+        
 
-      <Outlet></Outlet>
+
+        </Container>
+
+      </div>
+       
+      <div className="flex ">
+        <div className="xl:w-[20%] h-full min-h-screen bg-primary w-0">
+          <div className="bg-primary h-full w-full text-fourth py-3">
+          <div className="flex justify-center items-center pt-5">
+            <div className="relative group">
+              <img
+                className="w-[50px] h-[50px] bg-slate-500 object-cover rounded-full"
+                src="https://source.unsplash.com/300x300/?profile"
+                alt=""
+              />
+              <span className="h-3 w-3 bg-white p-[2px] shadow-[0px_2px_8px_0px_rgba(99,99,99,0.4)]  group-hover:-rotate-180 duration-500 absolute rounded-full -bottom-2 left-[50%] -translate-x-1/2">
+                <svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#60A5FA"
+                >
+                  {" "}
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>{" "}
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>{" "}
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <title></title>{" "}
+                    <g id="Complete">
+                      <g data-name="add" id="add-2">
+                        <g>
+                          {" "}
+                          <line
+                            fill="none"
+                            stroke="#60A5FA"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            x1="12"
+                            x2="12"
+                            y1="19"
+                            y2="5"
+                          ></line>
+                          <line
+                            fill="none"
+                            stroke="#60A5FA"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            x1="5"
+                            x2="19"
+                            y1="12"
+                            y2="12"
+                          ></line>
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 mt-12 ">
+            <h2 className="px-3 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              Statistics <FaArrowRight />
+            </h2>
+            <h2 className="px-3 mt-2 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              Money recieve <FaArrowRight />
+            </h2>
+            <h2 className="px-3 mt-2 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              Meal list <FaArrowRight />
+            </h2>
+            <h2 className="px-3 mt-2 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              Add Meal Day <FaArrowRight />
+            </h2>
+            <h2 className="px-3 mt-2 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              All Members <FaArrowRight />
+            </h2>
+          </div>
+        </div>
+          </div>
+          <div className="xl:w-[80%] w-full bg-fourth">
+          <Outlet></Outlet>
+          </div>
+        </div>
+
+
+ 
 
       {/* Drawer */}
       <Drawer
@@ -55,20 +146,80 @@ const Layout = () => {
         className=""
       >
         <div className="bg-primary h-full w-full text-fourth py-3">
-            
-            <div className="flex justify-center items-center pt-5">
+          <div className="flex justify-center items-center pt-5">
             <div className="relative group">
-                <img
-                  className="w-[50px] h-[50px] bg-slate-500 object-cover rounded-full"
-                  src="https://source.unsplash.com/300x300/?profile"
-                  alt=""
-                />
+              <img
+                className="w-[50px] h-[50px] bg-slate-500 object-cover rounded-full"
+                src="https://source.unsplash.com/300x300/?profile"
+                alt=""
+              />
               <span className="h-3 w-3 bg-white p-[2px] shadow-[0px_2px_8px_0px_rgba(99,99,99,0.4)]  group-hover:-rotate-180 duration-500 absolute rounded-full -bottom-2 left-[50%] -translate-x-1/2">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#60A5FA"> <g id="SVGRepo_bgCarrier" strokeWidth="0"></g> <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g> <g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"><g data-name="add" id="add-2"><g> <line fill="none" stroke="#60A5FA" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12" x2="12" y1="19" y2="5"></line><line fill="none" stroke="#60A5FA" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="5" x2="19" y1="12" y2="12"></line></g></g></g></g>
-                        </svg>
-                    </span>
-              </div>
+                <svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#60A5FA"
+                >
+                  {" "}
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>{" "}
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>{" "}
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <title></title>{" "}
+                    <g id="Complete">
+                      <g data-name="add" id="add-2">
+                        <g>
+                          {" "}
+                          <line
+                            fill="none"
+                            stroke="#60A5FA"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            x1="12"
+                            x2="12"
+                            y1="19"
+                            y2="5"
+                          ></line>
+                          <line
+                            fill="none"
+                            stroke="#60A5FA"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            x1="5"
+                            x2="19"
+                            y1="12"
+                            y2="12"
+                          ></line>
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </span>
             </div>
+          </div>
+          <div className="flex flex-col gap-2 mt-12 ">
+            <h2 className="px-3 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              Statistics <FaArrowRight />
+            </h2>
+            <h2 className="px-3 mt-2 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              Money recieve <FaArrowRight />
+            </h2>
+            <h2 className="px-3 mt-2 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              Meal list <FaArrowRight />
+            </h2>
+            <h2 className="px-3 mt-2 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              Add Meal Day <FaArrowRight />
+            </h2>
+            <h2 className="px-3 mt-2 text-lg font-medium border-b pb-3 border-fourth flex items-center duration-200 transition-all hover:gap-4 gap-2">
+              All Members <FaArrowRight />
+            </h2>
+          </div>
         </div>
       </Drawer>
     </div>
