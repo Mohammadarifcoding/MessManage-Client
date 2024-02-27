81f350e47ccf2b60ef053415490d62b2050e037e
Mohammad Arif <programmermdarif@gmail.com>
2024-02-27 09:17:21 +0600

    Updated the tailwind configuration

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -9,6 +9,7 @@ import { FaArrowRight } from "react-icons/fa6";
 import { FaMoneyBillTransfer } from "react-icons/fa6";
 import { MdChecklistRtl } from "react-icons/md";
 import { FaClipboardUser } from "react-icons/fa6";
+import { MdOutlineManageAccounts } from "react-icons/md";
 const Layout = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleDrawer = () => {
@@ -50,25 +51,27 @@ const Layout = () => {
           <div className=" flex justify-between  items-center ">
             {/* Navbar */}
             <div className=" px-3 py-4  text-fourth">
-              <div className="flex gap-3 items-center">
-                <img className="w-[40px]" src="/MassManageLogo.png" alt="" />
-                <h2 className="text-xl">MassManage</h2>
+              <div className="flex gap-2 items-center">
+                <div className="text-[30px] text-[#007AFF]">
+                  <MdOutlineManageAccounts />
+                </div>
+                <h2 className="sm:text-xl text-lg font-bold">MassManage</h2>
               </div>
             </div>
             <div
               onClick={toggleDrawer}
-              className="  h-full flex items-center gap-2 hover:bg-secondary p-2 rounded-3xl"
+              className="  h-full flex items-center sm:gap-2 gap-1 hover:bg-themeSecondColor p-2 rounded-3xl"
             >
               <div className="relative group">
                 <img
-                  className="w-[50px] h-[50px] bg-slate-500 object-cover rounded-full"
+                  className="sm:w-[50px] w-[35px] sm:h-[50px] h-[35px] bg-slate-500 object-cover rounded-full"
                   src="https://source.unsplash.com/300x300/?profile"
                   alt=""
                 />
                 <span className="h-4 w-4 bg-green-500 absolute rounded-full bottom-2 right-0 border-[3px] border-white"></span>
                 <span className="h-4 w-4 bg-green-500 absolute rounded-full bottom-2 right-0 animate-ping"></span>
               </div>
-              <div className="text-[18px] text-white">
+              <div className="sm:text-[18px] text-themeFirstColor">
                 <IoIosArrowDown />
               </div>
             </div>
@@ -226,21 +229,21 @@ const Layout = () => {
             </div>
           </div>
           <div className="flex flex-col gap-2 mt-12 ">
-          {url.map((item) => (
-                <Link key={item.UrlName} to={item.baseUrl}>
-                  <h2 className="px-3 text-lg font-medium border-b pb-3 group flex  items-center duration-200 transition-all hover:gap-4 gap-2">
-                    <span className="flex gap-2 items-center font-semibold">
-                      <span className="text-[#007AFF] font-bold">
-                        {item.icon}
-                      </span>{" "}
-                      {item.UrlName}
+            {url.map((item) => (
+              <Link key={item.UrlName} to={item.baseUrl}>
+                <h2 className="px-3 text-lg font-medium border-b pb-3 group flex  items-center duration-200 transition-all hover:gap-4 gap-2">
+                  <span className="flex gap-2 items-center font-semibold">
+                    <span className="text-[#007AFF] font-bold">
+                      {item.icon}
                     </span>{" "}
-                    <span className="p-2 bg-[#E6F2FF] text-[#007AFF] ml-auto duration-500 transition-all mr-6 group-hover:mr-0 rounded-full">
-                      <FaArrowRight />
-                    </span>
-                  </h2>
-                </Link>
-              ))}
+                    {item.UrlName}
+                  </span>{" "}
+                  <span className="p-2 bg-[#E6F2FF] text-[#007AFF] ml-auto duration-500 transition-all mr-6 group-hover:mr-0 rounded-full">
+                    <FaArrowRight />
+                  </span>
+                </h2>
+              </Link>
+            ))}
           </div>
         </div>
       </Drawer>
