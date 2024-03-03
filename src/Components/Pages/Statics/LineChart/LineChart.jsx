0834796fe0d163cd98a08d54115beafbf009c730
Mohammad Arif <programmermdarif@gmail.com>
2024-03-03 23:04:04 +0600

    Configured the meal manage

--- src/Components/Pages/Statics/LineChart/LineChart.jsx
+++ src/Components/Pages/Statics/LineChart/LineChart.jsx
@@ -3,35 +3,45 @@ import ReactApexChart from "react-apexcharts";
 import getDateArray from "../../../../Custom/GetDateArray";
 
 const LineChart = () => {
-
-
   // Print or use the current month name
 
   const series = [
     {
       name: "Meal month Chart",
-      data:[87, 56, 93, 41, 64, 74, 25, 82, 38, 95, 59, 31, 46, 68, 79, 23, 88, 72, 60, 27, 90, 48, 22, 55, 67, 43, 76]
-      ,
+      data: [
+        87, 56, 93, 41, 64, 74, 25, 82, 38, 95, 59, 31, 46, 68, 79, 23, 88, 72,
+        60, 27, 90, 48, 22, 55, 67, 43, 76,
+      ],
     },
   ];
 
   const options = {
     chart: {
+      type: "area",
+      stacked: false,
       height: 350,
-      type: "line",
       zoom: {
-        enabled: false,
+        type: "x",
+        enabled: true,
+        autoScaleYaxis: true,
+      },
+      toolbar: {
+        autoSelected: "zoom",
       },
     },
     dataLabels: {
       enabled: false,
     },
+    markers: {
+      size: 0,
+    },
     stroke: {
       curve: "straight",
     },
     title: {
-      text: "Product Trends by Month",
+      text: "Meal month Chart",
       align: "left",
+      
     },
     grid: {
       row: {
@@ -39,6 +49,7 @@ const LineChart = () => {
         opacity: 0.5,
       },
     },
+
     xaxis: {
       categories: getDateArray(),
     },
